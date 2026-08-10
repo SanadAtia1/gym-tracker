4a58172a8f6dfff116b916e91a74d7140518bf66
Sanad Mac <sanadatia.07@gmail.com>
2026-08-09 21:54:18 -0700

    tables

--- app.js
+++ app.js
@@ -3,15 +3,60 @@ const app = express();
 const database = require('better-sqlite3');
 const db = new database('gym.db');
 
-db.exec('CREATE TABLE IF NOT EXISTS testTable (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)');
-const insert = db.prepare('INSERT INTO testTable (name, password) VALUES (?, ?)');
-// insert.run('sanad', '123456');
-// insert.run('john', '654321');
-const deleteStatement = db.prepare('DELETE FROM testTable WHERE id = ?');
-deleteStatement.run(5);
-const update = db.prepare('UPDATE testTable SET ID = ? WHERE name = ?');
-update.run(1, 'testing');
-const rows = db.prepare('SELECT * FROM testTable').all();
+// db.exec('CREATE TABLE IF NOT EXISTS sessions (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL)');
+// const insert = db.prepare('INSERT INTO days (name) VALUES (?)');
+
+// db.exec('CREATE TABLE IF NOT EXISTS days (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL)');
+// const insert = db.prepare('INSERT INTO days (name) VALUES (?)');
+// repopulate for version control
+// insert.run('Day 1');
+// insert.run('Day 2');
+// insert.run('Day 3');
+// insert.run('Day 4');
+// insert.run('Day 5');
+// insert.run('Day 6');
+
+db.exec('CREATE TABLE IF NOT EXISTS exercises (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, reps TEXT)');
+const insert2 = db.prepare('INSERT INTO exercises (name, reps) VALUES (?, ?');
+// repopulate for version control
+insert2.run('Back Squat', '4x5');
+// insert.run('Bench Press', '4x5');
+// insert.run('Barbell Row', '3x8');
+// insert.run('DB Overhead Press', '3x10');
+// insert.run('Deadlift', '4x5');
+// insert.run('Chin-ups', '4x8');
+// insert.run('Incline DB Press', '3x8');
+// insert.run('Curls', '3x12');
+// insert.run('Bulgarian Split Squat', '3x8');
+// insert.run('Overhead Press', '4x6');
+// insert.run('Romanian Deadlift', '3x10');
+// insert.run('Cable Row', '3x12');
+// insert.run('Romanian Deadlift', '3x8');
+// insert.run('Pull-ups', '3x8');
+// insert.run('Bulgarian Split Squat', '3x12');
+// insert.run('Incline DB Press', '3x12');
+// insert.run('Lat Pulldown', '3x15');
+// insert.run('Lateral Raise', '3x15');
+// insert.run('Cable Curls +  Tricep Pushdowns', '3x15');
+// insert.run('DB Snatch', '3x6');
+// insert.run('Chin-ups', '3x10');
+
+// db.exec('CREATE TABLE IF NOT EXISTS day_exercises_junction (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL)');
+// const insert = db.prepare('INSERT INTO days (name) VALUES (?)');
+
+// db.exec('CREATE TABLE IF NOT EXISTS log (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL)');
+// const insert = db.prepare('INSERT INTO days (name) VALUES (?)');
+
+const rows2 = db.prepare('SELECT * FROM exercises').all();
+console.log(rows);
+
+
+
+//const deleteStatement = db.prepare('DELETE FROM testTable WHERE id = ?');
+//deleteStatement.run(3);
+//const update = db.prepare('UPDATE testTable SET name = ? WHERE ID = ?');
+//update.run('updated', 2);
+const rows = db.prepare('SELECT * FROM days').all();
 console.log(rows);
 
 // app.get('/', (req, res) => {
