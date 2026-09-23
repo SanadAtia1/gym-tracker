44657c979a472bf10d498ff4b9fe72f50e5548e4
Sanad Mac <sanadatia.07@gmail.com>
2026-09-22 20:25:15 -0700

    new features - session view updted del added + more

--- app.js
+++ app.js
@@ -11,40 +11,23 @@ console.log(rows3);
 app.use(express.static('public'));
 app.use(express.json());
 
-// const testQ = db.prepare(`
-//     WITH ranked AS (
-//         SELECT e.exercise, e.exercise_id, l.weight, l.reps,
-//             ROW_NUMBER() OVER (PARTITION BY e.exercise_id ORDER BY s.date DESC) AS rn
-//         FROM exercises e
-//         JOIN junction j ON j.exerciseRef = e.exercise_id 
-//         LEFT JOIN logs l ON l.exerciseRef = e.exercise_id
-//         LEFT JOIN sessions s ON s.session_id = l.sessionRef
-//         WHERE j.day = 2
-//     )
-//         SELECT *
-//         FROM ranked
-//         WHERE rn = 1;
-//     `).all();
-
-    // console.log(testQ);
-
-// view workouts on a given day
+// view exercises by day
 app.get('/days/:day', (req, res) => {
     const dayNum = req.params.day;
     if (dayNum > 0 && dayNum < 7) {
         const dayQuery = db.prepare(`
-        WITH ranked AS (
-        SELECT e.exercise, e.exercise_id, l.weight, l.reps,
-            ROW_NUMBER() OVER (PARTITION BY e.exercise_id ORDER BY s.date DESC) AS rn
-        FROM exercises e
-        JOIN junction j ON j.exerciseRef = e.exercise_id 
-        LEFT JOIN logs l ON l.exerciseRef = e.exercise_id
-        LEFT JOIN sessions s ON s.session_id = l.sessionRef
-        WHERE j.day = ?
-    )
-        SELECT *
-        FROM ranked
-        WHERE rn = 1;
+            WITH ranked AS (
+            SELECT e.exercise, e.exercise_id, l.weight, l.reps,
+            ROW_NUMBER() OVER (PARTITION BY e.exercise_id ORDER BY s.date DESC, l.id DESC) AS rn
+            FROM exercises e
+            JOIN junction j ON j.exerciseRef = e.exercise_id 
+            LEFT JOIN logs l ON l.exerciseRef = e.exercise_id
+            LEFT JOIN sessions s ON s.session_id = l.sessionRef
+            WHERE j.day = ?
+            )
+            SELECT *
+            FROM ranked
+            WHERE rn = 1;
         `).all(dayNum);
     
         res.json(dayQuery);
@@ -53,37 +36,123 @@ app.get('/days/:day', (req, res) => {
     }
 });
 
-// start new session
+// create new session
 app.post('/sessions', (req, res) => {
     const day = req.body.day;
-    const createSession = db.prepare('INSERT INTO sessions (dayLogged) VALUES (?)');
-    const sessID = createSession.run(day);
-    res.json(sessID.lastInsertRowid);
+    const dupSess = db.prepare('SELECT * FROM sessions s WHERE date = CURRENT_DATE').all();
+    if (dupSess.length > 0) {
+        res.json({
+            duplicate: true,
+            message: 'Delete existing session to create new one'
+        });
+        return;
+    }
+    const createSession = db.prepare('INSERT INTO sessions (dayLogged) VALUES (?)').run(day);
+    res.json({
+        duplicate: false,
+        message: 'Session created',
+        sessID: createSession.lastInsertRowid
+    });
   });
 
-// new log entries
+// create log entries
 app.post('/logs', (req, res) => {
     const { sessionID, exerciseID, weight, reps } = req.body;
-    const createLog = db.prepare('INSERT INTO logs (sessionRef, exerciseRef, weight, reps) VALUES (?, ?, ?, ?)');
-    const logID = createLog.run(sessionID, exerciseID, weight, reps);
-    res.json(logID.lastInsertRowid);
+    if (weight.trim() === '' || reps.trim() === '') {
+        res.json({ 
+            success: false,
+            message:'Invalid input(s)'
+        });
+        return;
+    }
+    const createLog = db.prepare('INSERT INTO logs (sessionRef, exerciseRef, weight, reps) VALUES (?, ?, ?, ?)').run(sessionID, exerciseID, weight, reps);
+    res.json({
+        success: true,
+        logID: createLog.lastInsertRowid
+        
+    });
   });
 
-// view logs on a given date
+// view logs by date
 app.get('/sessions/:date', (req, res) => {
-    const sessDate = req.params.date;
-    const viewSess = db.prepare(`
-        SELECT e.exercise, l.weight, l.reps, s.date, l.id
-        FROM exercises e
-        JOIN logs l ON l.exerciseRef = e.exercise_id
-        JOIN sessions s ON s.session_id = l.sessionRef
-        WHERE s.date = ?
-        `).all(sessDate);
-
-        res.json(viewSess);
+    const sessionDate = req.params.date;
+    const checkSession = db.prepare('SELECT * FROM sessions WHERE date = ?').get(sessionDate);
+    if (checkSession) {
+        const printLogInfo = db.prepare(`
+            SELECT e.exercise, l.weight, l.reps, s.date, l.id, s.session_id, s.dayLogged
+            FROM exercises e
+            JOIN logs l ON l.exerciseRef = e.exercise_id
+            JOIN sessions s ON s.session_id = l.sessionRef
+            WHERE s.date = ?
+        `).all(sessionDate);
+        
+        if (printLogInfo.length > 0) { 
+            res.json({
+                sessionExists: true,
+                logsExist: true,
+                logs: printLogInfo,
+            });
+        } else {
+            res.json({
+                sessionExists: true,
+                logsExist: false,
+                session: checkSession
+            });
+        }
+        return;
+    } else {
+        res.json({
+            sessionExists: false,
+            message: 'No sessiom created for this date'
+        })
+    }
 });
 
-// delete log entry
+app.get('/sessions/:id/exercises', (req, res) => {
+    const sessionID = req.params.id;
+    const printEmptyLogs = db.prepare(`
+        SELECT e.exercise, l.weight, l.reps, s.date, l.id, s.dayLogged
+        FROM exercises e
+        JOIN junction j ON j.exerciseRef = e.exercise_id 
+        LEFT JOIN logs l ON l.exerciseRef = e.exercise_id AND l.sessionRef = ?
+        JOIN sessions s ON s.session_id = l.sessionRef
+        WHERE s.session_id = ?
+    `).all(sessionID, sessionID);
+
+    console.log(printEmptyLogs);
+});
+
+
+
+// delete empty sessions
+app.delete('/sessions/:id', (req, res) => {
+    const sessionID = req.params.id;
+    const hasLogs = db.prepare('SELECT * FROM logs WHERE sessionRef = ?').all(sessionID);
+    if (hasLogs.length > 0) {
+        res.json({
+            logsExist: true,
+            message: 'Cannot delete session containing logs'
+        });
+        return;
+    }
+    const deleteSession = db.prepare('DELETE FROM sessions WHERE session_id = ?');
+    const delInfo = deleteSession.run(sessionID);
+
+    if (delInfo.changes > 0){
+        res.json({
+            logsExist: false,
+            message: `Session ${sessionID} deleted`
+        });
+    } else {
+        res.json({
+            logsExist: false,
+            message: 'No session deleted'
+        });
+    }
+
+});
+
+// delete logs
 app.delete('/logs/:id', (req, res) => {
     const logID = req.params.id;
     const deleteLog = db.prepare('DELETE FROM logs WHERE id = ?');
@@ -96,7 +165,7 @@ app.delete('/logs/:id', (req, res) => {
     }
 });
 
-// update existing log entry
+// update logs
 app.patch('/logs/:id', (req, res) => {
     const logID = req.params.id;
     const { weight, reps } = req.body;
